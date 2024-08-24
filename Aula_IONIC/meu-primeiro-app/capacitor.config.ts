a60b2cef98b1adc924785269e7e01534a4c7fcde
Desenvolvimento <dsadm@educ501.local>
2024-08-24 10:13:55 -0300

    IONIC

---
+++ Aula_IONIC/meu-primeiro-app/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'MeuPrimeiroApp',
+  webDir: 'www'
+};
+
+export default config;
